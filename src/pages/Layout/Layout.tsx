06b2ede46ffb17a8756069de6fbec84a03394d9a
bynilow <bynilow@mail.ru>
2025-09-14 04:48:17 +0500

    added loggers

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -74,10 +74,12 @@ const Layout = () => {
         if (userId && userId !== undefined) {
             document.cookie = `user_id=${userId}; path=/; samesite=none; secure; max-age=2592000`;
             console.log('Cookie set with userId:', userId);
+            console.log('cookies if userid exists', document.cookie)
         } else {
             console.log('Invalid userId for cookie:', userId);
             // Удаляем некорректную куку, если она существует
             document.cookie = 'user_id=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;';
+            console.log('cookies if userid not exists', document.cookie)
         }
     }, [userData])
 
