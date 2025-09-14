27840534c73ea993a305da4da3d3ae0d670af265
bynilow <bynilow@mail.ru>
2025-09-14 06:17:15 +0500

    fixed last visited date

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -110,7 +110,7 @@ const Layout = () => {
     const { openModal } = useModal();
 
     useEffect(() => {
-        if (userData && perSecond && !isAcceptedOfflineIncome) {
+        if (userData && perSecond && !isAcceptedOfflineIncome && userData.last_visited_date) {
             const currentDateTime = new Date(new Date().toUTCString().replace('GMT', '')).getTime();
             const lastVisitedDateTime = new Date(userData.last_visited_date).getTime();
             const dateTimeDiff = Math.abs(currentDateTime - lastVisitedDateTime) / 1000;
