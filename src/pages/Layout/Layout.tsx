5f510d79b87725899fe2c9802599dca2be888773
bynilow <bynilow@mail.ru>
2025-09-14 16:22:21 +0500

    return 1337

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -56,8 +56,8 @@ const Layout = () => {
         e.preventDefault();
     });
 
-    const userId = tg?.initDataUnsafe?.user?.id || 55355;
-    const userName = tg?.initDataUnsafe?.user?.username || 'small_id';
+    const userId = tg?.initDataUnsafe?.user?.id || 1337;
+    const userName = tg?.initDataUnsafe?.user?.username || 'test_user';
 
     const { userData, userError, userIsLoading, createUser } = useUser(userId, userName);
     const { boostIsLoading, boostsError, boostsData } = useBoosts();
