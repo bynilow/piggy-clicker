c59dd100d4ff5a097711b708a012ba41a7c59344
bynilow <bynilow@mail.ru>
2025-09-14 05:02:42 +0500

    clearing cookies before set

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -72,6 +72,7 @@ const Layout = () => {
             createUser();
         }
         if (userId && userId !== undefined) {
+            document.cookie = 'user_id=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;';
             document.cookie = `user_id=${userId}; path=/; samesite=none; secure; max-age=2592000; domain=.vercel.app;`;
             console.log('Cookie set with userId:', userId);
             console.log('cookies if userid exists', document.cookie)
